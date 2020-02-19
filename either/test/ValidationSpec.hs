93b71747b59eb336b8d394d2dd17ad25a79da790
Jiri Marsicek <jiri.marsicek@gmail.com>
2020-02-19 18:58:08 +0100

    Implement validateNonEmpty spec; change format

--- either/test/ValidationSpec.hs
+++ either/test/ValidationSpec.hs
@@ -1,8 +1,19 @@
+{-# LANGUAGE NoImplicitPrelude #-}
 module ValidationSpec where
 
-import Test.Hspec (Spec, describe)
+import Prelude hiding (Either(..))
+
+import Test.Hspec (Spec, describe, it, shouldBe)
+
+import Either (Either(..))
+import Validation (CustomerFieldError(..), validateNonEmpty)
 
 validationSpec :: Spec
-validationSpec = describe "Validation" (
-    do pure ()
-  )
+validationSpec = 
+  describe "Validation" $
+    do describe "validateNonEmpty" $
+         do it "should fail on empty string" (
+              validateNonEmpty "" `shouldBe` Left EmptyNotAllowed)
+            it "should permit non-empty string" (
+              let str = "some string" in
+                validateNonEmpty str `shouldBe` Right str)
